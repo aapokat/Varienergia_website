539ff2e1b62e37428f3ba8a9c062e51d441a3ed4
aapokat <aapo.katajala@gmail.com>
2024-05-24 13:11:05 +0300

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -1,4 +1,19 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Enable CORS
+header("Access-Control-Allow-Origin: *"); // Allow requests from any origin. Use specific origin in production, e.g., "http://yourdomain.com"
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+    
+    // Handle preflight request (OPTIONS)
+    if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+        http_response_code(200);
+        exit();
+    }
+
 // Database configuration
 $servername = "localhost:3306"; // e.g., localhost
 $username = "variener_kayttis";
